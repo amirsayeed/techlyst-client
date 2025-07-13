6b5941b1391daa5a392c215a3523d7a866b224ac
amirsayeed <68963306+amirsayeed@users.noreply.github.com>
2025-07-13 23:55:26 +0600

    dashboard layout designed and user dashboard links added with relevant icons

--- src/Router/Routes.jsx
+++ src/Router/Routes.jsx
@@ -6,6 +6,11 @@ import Home from "../pages/Home/Home/Home";
 import AuthLayout from "../layouts/AuthLayout";
 import Login from "../pages/Authentication/Login/Login";
 import Register from "../pages/Authentication/Register/Register";
+import DashboardLayout from "../layouts/DashboardLayout";
+import PrivateRoute from "../routes/PrivateRoute";
+import MyProfile from "../pages/Dashboard/MyProfile/MyProfile";
+import AddProduct from "../pages/Dashboard/AddProduct/AddProduct";
+import MyProducts from "../pages/Dashboard/MyProducts/MyProducts";
 
 export const router = createBrowserRouter([
   {
@@ -30,6 +35,24 @@ export const router = createBrowserRouter([
             path: 'register',
             Component: Register
         }
+    ]
+  },
+  {
+    path: '/dashboard',
+    element: <PrivateRoute><DashboardLayout/></PrivateRoute>,
+    children: [
+      {
+        path: 'myProfile',
+        Component: MyProfile
+      },
+      {
+        path: 'addProduct',
+        Component: AddProduct
+      },
+      {
+        path: 'myProducts',
+        Component: MyProducts
+      }
     ]
   }
 ]);
