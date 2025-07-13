38a8ac04752b31ab6277992f36e90b32df26ddc5
amirsayeed <68963306+amirsayeed@users.noreply.github.com>
2025-07-13 12:31:55 +0600

    firebase project setup, credentials hide with .env and authprovider setup completed

---
+++ src/provider/AuthProvider.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react';
+import { AuthContext } from './AuthContext';
+import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from 'firebase/auth';
+import { auth } from '../firebase/firebase.init';
+const googleProvider = new GoogleAuthProvider();
+const AuthProvider = ({children}) => {
+    const [loading,setLoading] = useState(true);
+    const [user,setUser] = useState('');
+
+    const signUp = (email,password) =>{
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth,email,password);
+    }
+
+    const signIn = (email,password) =>{
+        setLoading(true);
+        return signInWithEmailAndPassword(auth,email,password);
+    }
+
+    const googleSignIn = () =>{
+        setLoading(true);
+        return signInWithPopup(auth, googleProvider);
+    }
+
+     const logOut = () =>{
+        setLoading(true);
+        return signOut(auth);
+    }
+
+    const updateUser = (updatedData) => {
+        setLoading(true);
+        return updateProfile(auth.currentUser, updatedData);
+    };
+
+     useEffect(()=>{
+        const unsubscribe = onAuthStateChanged(auth,currentUser=>{
+            console.log(currentUser);
+            setUser(currentUser);
+            setLoading(false);
+        })
+        return ()=>{
+            unsubscribe();
+        }
+    },[])
+
+    const userInfo = {
+        signUp,
+        signIn,
+        googleSignIn,
+        logOut,
+        loading,
+        user,
+        setUser,
+        updateUser
+    }
+
+    return (
+        <AuthContext value={userInfo}>
+            {children}
+        </AuthContext>
+    );
+};
+
+export default AuthProvider;
